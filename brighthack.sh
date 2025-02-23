4097cfc39ee0b0cafe4aaaa87f62d492dcfb07d3
frost-1256 <habanero.iori@gmail.com>
2025-02-23 20:03:22 +0900

    fix brighthack.sh

--- brighthack.sh
+++ brighthack.sh
@@ -1,19 +1,17 @@
 #!/system/bin/sh
 
-# This script checks the current brightness
-# and offsets the values. It could surely 
-# be made more robust. Currently un-tested 
-# on Oxygen OS.
-
 BRIGHTNESS_FILE=/sys/class/backlight/panel0-backlight/brightness
 
-BRIGHTNESS=$(cat ${BRIGHTNESS_FILE})
+while true; do
+  BRIGHTNESS=$(cat ${BRIGHTNESS_FILE})
 
-#echo $BRIGHTNESS
+  # 明るさが8未満なら8に設定
+  if [ "$BRIGHTNESS" -lt 8 ]; then
+    echo 8 > ${BRIGHTNESS_FILE}
+  fi
 
-# 明るさが8未満なら8に設定
-if [ "$BRIGHTNESS" -lt 8 ]; then
-  echo 8 > ${BRIGHTNESS_FILE}
-fi
+
+done
 
 exit 0
+
