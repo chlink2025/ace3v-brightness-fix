389a2ae506f44349be72fd8795c1a454e4a775e6
frost-1256 <habanero.iori@gmail.com>
2025-02-24 14:59:14 +0900

    service.sh

--- service.sh
+++ service.sh
@@ -27,7 +27,7 @@ while true; do
   BRIGHTNESS=$(cat ${BRIGHTNESS_FILE})
 
   # 明るさが8未満なら8に設定
-  if [ "$BRIGHTNESS" -lt 8 ]; then
+  if [ "$BRIGHTNESS" -lt 12 ]; then
     echo 8 > ${BRIGHTNESS_FILE}
   fi
 
